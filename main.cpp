f01ff56cd0ba9d7e2c9b69875e07308b0abf7186
0ptim0 <optim000000@gmail.com>
2021-06-12 19:34:11 +0700

    Rename tasks

--- main.cpp
+++ main.cpp
@@ -2,30 +2,27 @@
 
 rcc Rcc1;
 adc Adc1;
-indicator Indicator1(IND_DIGIT_1, IND_PRECISION_0, IND_PERIOD_5_MS);
-indicator Indicator2(IND_DIGIT_1, IND_PRECISION_0, IND_PERIOD_5_MS);
+indicator Indicator1(IND_DIGIT_2, IND_PRECISION_1, IND_PERIOD_5_MS);
 //usart Usart2(USART2, 115200);
 
-float i1 = 0;
-float i2 = 0;
+float value;
 
-void vTask1(void *pvParameters) {
+void Indicator(void *pvParameters) {
     Indicator1.SetDigit(1, GPIOA, GPIO_PIN_8);
-    Indicator2.SetDigit(1, GPIOA, GPIO_PIN_9);
+    Indicator1.SetDigit(2, GPIOA, GPIO_PIN_9);
     Indicator1.Init();
-    Indicator2.Init();
     while(1){
-        Indicator1.Print(i1);
-        Indicator2.Print(i2);
+        Indicator1.Print(value);
     }
 }
 
-void vTask2(void *pvParameters) {
+void Measurement(void *pvParameters) {
     Adc1.Init(ADC1);
     while(1){
-        vTaskDelay(10);
-        i1 = Adc1.buf[0] * 3.3 / (0x0FFF);
-        i2 = Adc1.buf[1] * 3.3 / (0x0FFF);
+        vTaskDelay(1000);
+        value = Adc1.buf[0] * 3.3 / (0x0FFF);
+        vTaskDelay(1000);
+        value = Adc1.buf[1] * 3.3 / (0x0FFF);
     }
 }
 
@@ -47,8 +44,8 @@ int main(void) {
     HAL_Init();
     Rcc1.Init();
     //adc1.Init();
-    xTaskCreate(vTask1, "LED control", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-    xTaskCreate(vTask2, "Increment", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+    xTaskCreate(Indicator, "Indicator", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+    xTaskCreate(Measurement, "Measurement", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
     //xTaskCreate(vTask3, "Send", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
     vTaskStartScheduler();
     while(1){  
